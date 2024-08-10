9dd264c009c7f1fe5257eef184897596cdba4d04
joenetuban <joenetuban@gmail.com>
2024-08-10 12:53:44 +0800

    add button

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,7 @@
-import React, { useRef } from 'react';
-import emailjs from '@emailjs/browser';
-import Swal from 'sweetalert2';
-import './Contact.css';
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import Swal from "sweetalert2";
+import "./Contact.css";
 
 const Contact = () => {
   const form = useRef();
@@ -10,19 +10,24 @@ const Contact = () => {
     e.preventDefault();
 
     emailjs
-      .sendForm('service_chqsxja', 'template_w1v2kcx', form.current, 'YdxotD56__QuUGD-c')
+      .sendForm(
+        "service_chqsxja",
+        "template_w1v2kcx",
+        form.current,
+        "YdxotD56__QuUGD-c"
+      )
       .then(
         () => {
           Swal.fire({
-            icon: 'success',
-            title: 'Email Sent',
-            text: 'Your message has been sent successfully!',
+            icon: "success",
+            title: "Email Sent",
+            text: "Your message has been sent successfully!",
           });
         },
         (error) => {
           Swal.fire({
-            icon: 'error',
-            title: 'Email Failed',
+            icon: "error",
+            title: "Email Failed",
             text: `Failed to send email: ${error.text}`,
           });
         }
@@ -36,28 +41,67 @@ const Contact = () => {
         <div className="row">
           <div className="col-md-6 contact-info">
             <h4>Get in Touch</h4>
-            <p>If you have any questions or just want to say hello, feel free to drop me a message. I'll get back to you as soon as I can!</p>
+            <p>
+              If you have any questions or just want to say hello, feel free to
+              drop me a message. I'll get back to you as soon as I can!
+            </p>
             <ul className="list-unstyled d-flex flex-column">
-              <li><i className="bi bi-phone-fill"></i> 0956 027 3240</li>
-              <li><i className="bi bi-envelope-fill"></i> joenetuban@gmail.com</li>
-              <li><i className="bi bi-geo-alt-fill"></i> Barangay Culiat, Quezon City, Philippines</li>
+              <li>
+                <i className="bi bi-phone-fill"></i> 0956 027 3240
+              </li>
+              <li>
+                <i className="bi bi-envelope-fill"></i> joenetuban@gmail.com
+              </li>
+              <li>
+                <i className="bi bi-geo-alt-fill"></i> Barangay Culiat, Quezon
+                City, Philippines
+              </li>
             </ul>
           </div>
           <div className="col-md-6 contact-form">
             <form ref={form} onSubmit={sendEmail} className="glass-effect">
               <div className="mb-3">
-                <label htmlFor="name" className="form-label">Name</label>
-                <input type="text" className="form-control" id="name" name="user_name" placeholder="Your Name" required />
+                <label htmlFor="name" className="form-label">
+                  Name
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="name"
+                  name="user_name"
+                  placeholder="Your Name"
+                  required
+                />
               </div>
               <div className="mb-3">
-                <label htmlFor="email" className="form-label">Email address</label>
-                <input type="email" className="form-control" id="email" name="user_email" placeholder="Your Email" required />
+                <label htmlFor="email" className="form-label">
+                  Email address
+                </label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  name="user_email"
+                  placeholder="Your Email"
+                  required
+                />
               </div>
               <div className="mb-3">
-                <label htmlFor="message" className="form-label">Message</label>
-                <textarea className="form-control" id="message" name="message" rows="4" placeholder="Your Message" required></textarea>
+                <label htmlFor="message" className="form-label">
+                  Message
+                </label>
+                <textarea
+                  className="form-control"
+                  id="message"
+                  name="message"
+                  rows="4"
+                  placeholder="Your Message"
+                  required
+                ></textarea>
               </div>
-              <button type="submit" className="btn btn-primary">Send Message</button>
+              <button type="submit" className="btn btn-primary">
+                Send Message
+              </button>
             </form>
           </div>
         </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import './Navbar.css';
+import React from "react";
+import "./Navbar.css";
 
 const Navbar = () => {
-
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
         {/* Brand Logo */}
         <div className="navbar-brand">
-          <img src="/images/logo2.png" alt="Joene's Logo" style={{ height: '50px' }} />
+          <img
+            src="/images/logo2.png"
+            alt="Joene's Logo"
+            style={{ height: "50px" }}
+          />
         </div>
         <button
           className="navbar-toggler"
@@ -23,12 +26,21 @@ const Navbar = () => {
         </button>
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div className="navbar-nav ms-auto">
-            <a className="nav-link active" aria-current="page" href="#">Home</a>
-            <a className="nav-link" href="#projects">Projects</a>
-            <a className="nav-link" href="#about-me">About</a>
-            <a className="nav-link" href="#tools">Tools</a>
-            <a className="nav-link" href="#contact">Contact</a>
-            
+            <a className="nav-link" aria-current="page" href="#">
+              <span className="box">Home</span>
+            </a>
+            <a className="nav-link" href="#projects">
+              <span className="box">Projects</span>
+            </a>
+            <a className="nav-link" href="#about-me">
+              <span className="box">About</span>
+            </a>
+            <a className="nav-link" href="#tools">
+              <span className="box">Tools</span>
+            </a>
+            <a className="nav-link" href="#contact">
+              <span className="box">Contact</span>
+            </a>
           </div>
         </div>
       </div>
